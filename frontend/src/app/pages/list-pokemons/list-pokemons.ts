6d16fae1c709ac88a5cb793f624698af7ea47782
Ferenc Dajka <fesath_3@hotmail.com>
2025-07-17 20:36:27 +0200

    pokemon nlist WIP

--- frontend/src/app/pages/list-pokemons/list-pokemons.ts
+++ frontend/src/app/pages/list-pokemons/list-pokemons.ts
@@ -1,11 +1,21 @@
 import { Component } from '@angular/core';
+import { Pokemon, PokemonService } from '../../../../api/pokemon';
 
 @Component({
   selector: 'app-list-pokemons',
   imports: [],
   templateUrl: './list-pokemons.html',
-  styleUrl: './list-pokemons.scss'
+  styleUrl: './list-pokemons.scss',
 })
 export class ListPokemons {
+  allPokemon: Pokemon[] = [];
 
+  constructor(private pokemonService: PokemonService) {}
+
+  ngOnInit(): void {
+    this.pokemonService.apiPokemonGet().subscribe((result) => {
+      this.allPokemon = result;
+      console.log(result);
+    });
+  }
 }
