59eca5a7e971beb133547142a645e4a29282c652
Ferenc Dajka <fesath_3@hotmail.com>
2025-07-17 21:13:54 +0200

    User list component

--- frontend/src/app/pages/list-users/list-users.ts
+++ frontend/src/app/pages/list-users/list-users.ts
@@ -1,11 +1,20 @@
 import { Component } from '@angular/core';
+import { User, UserService } from '../../../../api/user';
 
 @Component({
   selector: 'app-list-users',
   imports: [],
   templateUrl: './list-users.html',
-  styleUrl: './list-users.scss'
+  styleUrl: './list-users.scss',
 })
 export class ListUsers {
+  allUser: User[] = [];
 
+  constructor(private userService: UserService) {}
+
+  ngOnInit() {
+    this.userService.apiUserGet().subscribe((result) => {
+      this.allUser = result;
+    });
+  }
 }
