ccc4d62c294e953ee49b8970af2afe67de88d9b9
Ferenc Dajka <fesath_3@hotmail.com>
2025-07-21 11:37:51 +0200

    completed edit/new user

--- frontend/src/app/pages/edit-user/edit-user.ts
+++ frontend/src/app/pages/edit-user/edit-user.ts
@@ -6,8 +6,8 @@ import {
   ReactiveFormsModule,
   Validators,
 } from '@angular/forms';
-import { Router } from '@angular/router';
-import { UserService } from '../../../../api/user';
+import { ActivatedRoute, Router } from '@angular/router';
+import { User, UserService } from '../../../../api/user';
 
 @Component({
   selector: 'app-edit-user',
@@ -17,11 +17,15 @@ import { UserService } from '../../../../api/user';
 })
 export class EditUser {
   form!: FormGroup;
+  idOrAction: string | null = null;
+  isCreate: boolean = false;
+  userId: number | undefined;
 
   constructor(
     private fb: FormBuilder,
     private userService: UserService,
-    private router: Router
+    private router: Router,
+    private route: ActivatedRoute
   ) {}
 
   ngOnInit(): void {
@@ -30,14 +34,45 @@ export class EditUser {
       nameJP: ['', Validators.required],
       age: [null, [Validators.required, Validators.min(0)]],
     });
+
+    this.route.paramMap.subscribe((params) => {
+      this.idOrAction = params.get('id'); // lehet '42' vagy 'create'
+      console.log(this.idOrAction);
+
+      if (this.idOrAction === 'create') {
+        this.isCreate = true;
+      } else {
+        this.isCreate = false;
+
+        const userId = Number(this.idOrAction);
+        if (!isNaN(userId)) {
+          this.userService.apiUserIdGet(userId).subscribe((user) => {
+            this.userId = user.id;
+
+            this.form.patchValue({
+              nameEN: user.nameEN,
+              nameJP: user.nameJP,
+              age: user.age,
+            });
+          });
+        }
+      }
+    });
   }
 
   submit(): void {
     if (this.form.invalid) return;
 
-    const newUser = this.form.value;
-    this.userService.apiUserPost(newUser).subscribe(() => {
-      this.router.navigate(['/users']);
-    });
+    const userToSend = this.form.value;
+
+    if (this.isCreate) {
+      this.userService.apiUserPost(userToSend).subscribe(() => {
+        this.router.navigate(['/users']);
+      });
+    } else {
+      this.userService.apiUserIdPut(this.userId!, userToSend).subscribe(() => {
+        this.router.navigate(['/users']);
+      });
+    }
   }
 }
